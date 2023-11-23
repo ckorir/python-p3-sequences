8ce89ccc5362dd78c0f3cb82526df984a1ef2a8a
ckorir <chrisruto.k@gmail.com>
2023-11-23 15:52:21 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        # Add numbers to the sequence until its length is equal to the specified length
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+
+        # Append the next number to the Fibonacci sequence
+        fibonacci_sequence.append(next_number)
+    print(fibonacci_sequence[:length])
     pass
